2714b850763d816d4784936890eb1815979d02ce
ALKHATIB <alkhatibibrahimtaha@gmail.com>
2022-12-25 23:38:49 +0100

    Commentaire

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,7 +1,10 @@
 package ma.enset;
-
+/*
+ * commentaire
+ */
 public class Test {
     public static void main(String[] args){
+        //.................................
         System.out.println("Hello World");
     } 
 }
